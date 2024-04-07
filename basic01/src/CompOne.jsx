f5d37c5f0a4abec014a8c2e49ab6c1640fb429df
susthir <susthir19march@gmail.com>
2024-04-07 23:49:41 +0530

    first react component

---
+++ basic01/src/CompOne.jsx
@@ -0,0 +1,9 @@
+function CompOne(){
+    return(
+        <div>   
+        <h3>This is my first react component</h3>
+        <button>Click Me To Learn More</button>
+        </div>
+    )
+}
+export default CompOne
